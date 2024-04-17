8da33709a1277a19ec9b2a416748634790474bff
Johann Schopplich <mail@johannschopplich.com>
2024-04-17 09:39:29 +0200

    ci: fix dir check

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'pathe'
 
 export default defineNuxtConfig({
   modules: [
-    existsSync(resolve(__dirname, '../dist/module.mjs')) ? 'nuxt-api-party' : '../src/module',
+    existsSync(resolve(__dirname, '../dist/module.mjs')) ? 'nuxt-prepare' : '../src/module',
   ],
 
   runtimeConfig: {
