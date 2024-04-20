8bd687a26a5d7485f7abdde620bdbc28e00f25fb
Johann Schopplich <mail@johannschopplich.com>
2024-04-20 12:04:59 +0200

    chore: add module to playground as a dependency

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,7 @@
-import { existsSync } from 'node:fs'
-import { resolve } from 'pathe'
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: [
-    existsSync(resolve(__dirname, '../dist/module.mjs')) ? 'nuxt-prepare' : '../src/module',
-  ],
+  modules: ['nuxt-prepare'],
 
   runtimeConfig: {
     public: {
